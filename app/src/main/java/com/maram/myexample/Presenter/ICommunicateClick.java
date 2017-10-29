97b99fd1b2015643ca90ba3301cf8b05ed6c9fa0
Marimuthu <marimuthu@okdollar.com>
2017-10-29 15:59:21 +0630

    Committed interface for set image.

---
+++ app/src/main/java/com/maram/myexample/Presenter/ICommunicateClick.java
@@ -0,0 +1,11 @@
+package com.maram.myexample.Presenter;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Marimuthu on 10/14/17.
+ */
+
+public interface ICommunicateClick {
+    void setImage(Bitmap image);
+}
